7691c1e0ae3a5fab6ed4aee91a277a611c4e24a9
MrBarker47 <jebarker7@gmail.com>
2024-11-13 23:37:16 -0500

    Update files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 5001;
+const port = 3001;
 
 
 app.get("/", (req, res) => {
@@ -12,12 +12,19 @@ app.post("/express", (req, res) => {
     res.send("Hello User");
 })
 
+
 app.get("/user/:userID/profile", (req, res) => {
     res.send(`Go through the users profile page for: ${req.params.userID}`);
 })
+
+let log = function(req, res, next) {
+    console.log("Charizard is the best")
+    next();
+};
+
+app.use(log);
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}.`)
 });
 
- 
