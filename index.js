1361203d1ddf298ef7f2cb5557d968c8b34be937
MrBarker47 <jebarker7@gmail.com>
2024-11-16 00:00:01 -0500

    Update files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
+//This code works in the terminal, but having issues with it on the browser so I have to figure that out. 
 const express = require('express')
 const app = express()
-const port = 3001;
+const port = 3000;
 
 
 app.get("/", (req, res) => {
@@ -17,6 +17,18 @@ app.get("/user/:userID/profile", (req, res) => {
     res.send(`Go through the users profile page for: ${req.params.userID}`);
 })
 
+
+//Template Engine from Mustache
+function renderHello() {
+    let template = document.getElementById('template').innerHTML;
+    let rendered = Mustache.render(template, { name: 'Luke' });
+    document.getElementById('target').innerHTML = rendered;
+  }
+
+  //Middleware
+
+
+
 let log = function(req, res, next) {
     console.log("Charizard is the best")
     next();
@@ -24,7 +36,10 @@ let log = function(req, res, next) {
 
 app.use(log);
 
+
+
+
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}.`)
 });
-
+ 
